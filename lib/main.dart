d25db1a37b6bd25c992d126977ddc583f6a1f91e
undeadlol1 <undeadlol1@mail.ru>
2021-06-06 23:13:39 +0500

    feat: :construction: adds zip file with new fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:fitness_app_mockup/states/viewer_state.dart';
-import 'package:fitness_app_mockup/storybook.dart';
-import 'package:flutter/material.dart';
-import 'package:fitness_app_mockup/screens/main_screen.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:fitness_app_mockup/storybook.dart';
+import 'package:fitness_app_mockup/states/viewer_state.dart';
+import 'package:fitness_app_mockup/screens/main_screen.dart';
 
 void main() {
-  final bool isStorybookEnabled = true;
+  final bool isStorybookEnabled = false;
   runApp(isStorybookEnabled ? Storybook() : MyApp());
 }
 
